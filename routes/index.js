dafe506516da0ccad1250900d9d1b3c9190a39c1
Minh Nguyen <minhnc.com@gmail.com>
2016-04-05 13:52:14 +0700

    Digits.logIn with callbackURL

--- routes/index.js
+++ routes/index.js
@@ -91,4 +91,72 @@ router.get('/digits', function (req, res, next) {
   });
 });
 
+/**
+ * GET Cannonball home page.
+ */
+router.get('/digits_callback', function (req, res, next) {
+  
+  	var oAuthEchoHeaders = req.query;
+  	var credentials = oAuthEchoHeaders['X-Verify-Credentials-Authorization'];
+    var apiUrl = oAuthEchoHeaders['X-Auth-Service-Provider'];
+    
+	  /// Copy from POST: /digits    
+    var verified = true;
+	  var messages = [];
+	
+	  // Verify the OAuth consumer key.
+	  if (credentials.indexOf('oauth_consumer_key="' + nconf.get('DIGITS_CONSUMER_KEY') + '"') == -1) {
+      verified = false;
+	    messages.push('The Digits API key does not match.');
+	  }
+	
+	  // Verify the hostname.
+	  var hostname = url.parse(apiUrl).hostname;
+	  if (hostname != 'api.digits.com' && hostname != 'api.twitter.com') {
+	    verified = false;
+	    messages.push('Invalid API hostname.');
+	  }
+	
+	  // Do not perform the request if the API key or hostname are not verified.
+	  if (!verified) {
+	    return res.send({
+	      phoneNumber: "",
+	      userID: "",
+	      error: messages.join(' ')
+	    });
+	  }
+	
+	  // Prepare the request to the Digits API.
+	  var options = {
+	    url: apiUrl,
+	    headers: {
+	      'Authorization': credentials
+	    }
+	  };
+    
+    
+    // Perform the request to the Digits API.
+	  request.get(options, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	      // Send the verified phone number and Digits user ID.
+	      var digits = JSON.parse(body);
+	      return res.send({
+	      	digits:		 digits,
+	        phoneNumber: digits.phone_number,
+	        userID: 	 digits.id_str,
+	        error: ''
+	      });
+	    } else {
+	      // Send the error.
+	      return res.send({
+	        phoneNumber: '',
+	        userID: '',
+	        error: error.message
+	      });
+	    }
+	  });
+	  /// Copy from POST: /digits >>>    
+    
+});
+
 module.exports = router;
